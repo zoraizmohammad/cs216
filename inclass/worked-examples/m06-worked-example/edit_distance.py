ac8b0cb70f0450b8b09fa9af5ec0cc3665faeaca
zoraizmohammad <zoraizmohammad@gmail.com>
2026-02-28 16:38:27 -0500

    completed question 1 and question 2

---
+++ inclass/worked-examples/m06-worked-example/edit_distance.py
@@ -0,0 +1,18 @@
+def edit_dist(str1, str2):
+    return(edit_helper(str1, str2, len(str1), len(str2)))
+    
+def edit_helper(str1, str2, m, n): 
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)] 
+    for i in range(m + 1): 
+        for j in range(n + 1): 
+            if i == 0: 
+                dp[i][j] = j    
+            elif j == 0: 
+                dp[i][j] = i    
+            elif str1[i-1] == str2[j-1]: 
+                dp[i][j] = dp[i-1][j-1] 
+            else: 
+                dp[i][j] = 1 + min(dp[i][j-1],       
+                                   dp[i-1][j],       
+                                   dp[i-1][j-1])
+    return dp[m][n]
